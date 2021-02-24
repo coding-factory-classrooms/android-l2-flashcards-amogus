4433b54572d3234950d7236b82b5dad722512b63
Laurent <laurent.sem@edu.itescia.fr>
2021-02-24 16:24:20 +0100

    Change lvl on questions

--- app/src/main/java/com/amogus/amogus/QuestionsListActivity.java
+++ app/src/main/java/com/amogus/amogus/QuestionsListActivity.java
@@ -22,9 +22,9 @@ public class QuestionsListActivity extends AppCompatActivity {
         questions = new ArrayList<>();
 
         for (int i = 0; i < 20; i++) {
-            questions.add(new Question("easy", "Quel est la date de sortie?", R.drawable.amongus_logo, new String[]{"15/06/2018", "25/09/2018", "15/12/2020"}));
-            questions.add(new Question("normal", "Quels sont les noms des deux équipes ?", R.drawable.amongus_logo, new String[]{"Imposters & Crewmates", "Alpha & Beta", "Innocents & Murders", "Dogs and cats"}));
-            questions.add(new Question("hard", "Quel est le nombre maximum de joueurs dans une partie ?", R.drawable.amongus_logo, new String[]{"10", "15", "5", "20", "6"}));
+            questions.add(new Question(0, "Quel est la date de sortie?", R.drawable.amongus_logo, new String[]{"15/06/2018", "25/09/2018", "15/12/2020"}));
+            questions.add(new Question(1, "Quels sont les noms des deux équipes ?", R.drawable.amongus_logo, new String[]{"Imposters & Crewmates", "Alpha & Beta", "Innocents & Murders", "Dogs and cats"}));
+            questions.add(new Question(2, "Quel est le nombre maximum de joueurs dans une partie ?", R.drawable.amongus_logo, new String[]{"10", "15", "5", "20", "6"}));
         }
 
         adapter = new QuestionAdapter(questions);
